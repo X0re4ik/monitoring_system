238b3a0162525787fe16ef17b8ec885a58739e84
Xoooore4ik <xoore4ik@gmail.com>
2023-10-14 16:56:36 +0500

    Simple code for sending data to HTTP request

--- src/main.cpp
+++ src/main.cpp
@@ -5,63 +5,38 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
+#include <ABSESP8266.h>
 
-class STA {
-public:
-
-    bool begin() {
-        wifiManager.resetSettings();
-        wifiManager.autoConnect("AutoConnectAP");
-    }
-private:
-
-    WiFiManager wifiManager;
-    String _last_password;
-    String _last_SSID;
-
-};
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
 
+#include "DeviceHTTPClient.h"
+
+#include <ThreadController.h>
+
+void sendDataAboutState() {
+    Serial.println(wifiOptions.wifi_name);
+}
+
+ThreadController thread_controller = ThreadController();
 
 
-
-class HTTPClient {
-
-};
+Thread* thread1 = new DeviceHTTPClientThread(&wifiOptions, &monitoringSystemServer);
+Thread* thread2 = new Thread(sendDataAboutState, 1000);
 
 
 
 void setup() {
-    // put your setup code here, to run once:
+
     Serial.begin(9600);
 
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-    wifiManager.resetSettings();
-    //reset saved settings
-    //wifiManager.resetSettings();
-    
-    //set custom ip for portal
-    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("AutoConnectAP");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-    
-    Serial.println(wifiManager.getWiFiPass(false));
-    Serial.println(wifiManager.getWiFiSSID(false));
-
-    
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+	thread_controller.add(thread1);
+	thread_controller.add(thread2);
+    WiFi.begin(wifiOptions.wifi_name, wifiOptions.password);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    
+    thread_controller.run();
 }
