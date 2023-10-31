0faf63e58dd84773736ea8951f1a5e5d2bf08bcb
Xoooore4ik <xoore4ik@gmail.com>
2023-10-31 20:36:44 +0500

    Реализована первая рабочая логика приложения

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,28 @@
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-
-//needed for library
+#include <ESP8266WiFi.h>          
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-
+#include <WiFiManager.h>
 #include <ABSESP8266.h>
-
-#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-
-
-#include "DeviceHTTPClient.h"
-
 #include <ThreadController.h>
 
-void sendDataAboutState() {
-    Serial.println(wifiOptions.wifi_name);
-}
+#include "SendThread.h"
 
 ThreadController thread_controller = ThreadController();
 
 
-Thread* thread1 = new MachineСonditionSendThread(&wifiOptions, &monitoringSystemServer);
-Thread* thread2 = new Thread(sendDataAboutState, 1000);
-
+Thread* thread1 = new MachineСonditionSendThread(&machine_condition);
 
 
 void setup() {
-
     Serial.begin(9600);
-
-	thread_controller.add(thread1);
-	thread_controller.add(thread2);
-    WiFi.begin(wifiOptions.wifi_name, wifiOptions.password);
+    thread_controller.add(thread1);
+    esp8266::AbstractDevice::waitConnectToWiFi();
 }
 
 void loop() {
     thread_controller.run();
+    esp8266::AbstractDevice::updateMachineCondition(&machine_condition);
+    delay(1000);
 }
