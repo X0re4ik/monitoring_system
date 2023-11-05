b3ac89f7350e4368fe21ebf5422670b1f60cdfcb
Xoooore4ik <xoore4ik@gmail.com>
2023-11-05 19:39:04 +0500

    Main logic

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,27 @@
 
 ThreadController thread_controller = ThreadController();
 
-
-Thread* thread1 = new MachineСonditionSendThread(&machine_condition);
-
+Thread* thread1 = new MachineConditionSendThread();
+Thread* thread2 = new QtyDetailsSendThread();
+Thread* thread3 = new TimeWorkSendThread();
 
 void setup() {
     Serial.begin(9600);
-    thread_controller.add(thread1);
+
+    thread_controller.add(thread1); // Контроль состояния станка
+    thread_controller.add(thread2); // Счетчик количества деталей
+    thread_controller.add(thread3); // Период работы
+
     esp8266::AbstractDevice::waitConnectToWiFi();
+    waitForSync();
+
+	Serial.println("UTC: " + UTC.dateTime());
 }
 
 void loop() {
     thread_controller.run();
-    esp8266::AbstractDevice::updateMachineCondition(&machine_condition);
-    delay(1000);
+
+    esp8266::AbstractDevice::updateMachineCondition(&machineCondition);
+    esp8266::AbstractDevice::updateQtyDetails(&qtyDetails);
+    esp8266::AbstractDevice::updateTimeWork(&timeWork);
 }
