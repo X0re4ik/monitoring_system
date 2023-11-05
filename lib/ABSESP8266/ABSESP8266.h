0cba8b5ef38f36c238da214fa890f96f4e64bfcd
Xoooore4ik <xoore4ik@gmail.com>
2023-11-05 19:38:45 +0500

    Implemented static methods for updating time and connection wait parameters

--- lib/ABSESP8266/ABSESP8266.h
+++ lib/ABSESP8266/ABSESP8266.h
@@ -20,13 +20,17 @@ enum DevicePins {
     START_WORK = 1,
     END_WORK = 2,
     INCREASE_QTY_GOOD_DETAILS = 3,
-    DECREASE_QTY_GOOD_DETAILS = 4
+    DECREASE_QTY_GOOD_DETAILS = 4,
+    TEMPERATURE = 0,
+    VIBRATION = 1,
+    SPINDLE_SPEED = 2
 };
 
 
 int _digitalRead(const DevicePins& pin) {
-    return digitalRead(pin);
+    return 1; //digitalRead(pin);
 }
+
 
 
 class AbstractDevice {
@@ -69,14 +73,23 @@ private:
         this->_mac_address = WiFi.macAddress();
         this->_connected_to_wifi = false;
         this->_status_mode = StatusMode::WAIT_CONNECT;
+
+        // pinMode(DevicePins::START_WORK, OUTPUT);
+        // pinMode(DevicePins::END_WORK, OUTPUT);
+        // pinMode(DevicePins::INCREASE_QTY_GOOD_DETAILS, OUTPUT);
+        // pinMode(DevicePins::DECREASE_QTY_GOOD_DETAILS, OUTPUT);
+        pinMode(DevicePins::TEMPERATURE, INPUT);
+        pinMode(DevicePins::VIBRATION, INPUT);
+        pinMode(DevicePins::SPINDLE_SPEED, INPUT);
+
     }
 
 public:
 
     static void updateMachineCondition(MachineCondition* machine_condition) {
-        machine_condition->spindle_speed = 1;
-        machine_condition->temperature = 1;
-        machine_condition->vibration = 1;
+        machine_condition->spindle_speed = digitalRead(DevicePins::SPINDLE_SPEED);
+        machine_condition->temperature = digitalRead(DevicePins::TEMPERATURE);
+        machine_condition->vibration = digitalRead(DevicePins::VIBRATION);
         machine_condition->dispatch_time = UTC.dateTime(ISO8601);
     }
 
@@ -85,12 +98,24 @@ public:
         int decrease_qty_good_details_button = _digitalRead(DevicePins::DECREASE_QTY_GOOD_DETAILS);
 
         if (increase_qty_good_details_button == HIGH) {
-            qty_details->qty_good_details++;
+            qty_details->qty_good_details += 1;
         }
-        else if (decrease_qty_good_details_button == HIGH) {
-            qty_details->qty_bad_details++;
+        if (decrease_qty_good_details_button == HIGH) {
+            qty_details->qty_bad_details += 1;
         }
         qty_details->dispatch_time = UTC.dateTime(ISO8601);
+    }
+
+    static void updateTimeWork(TimeWork* time_work) {
+        int start_work = _digitalRead(DevicePins::START_WORK);
+        int end_work = _digitalRead(DevicePins::END_WORK);
+
+        if (start_work == HIGH) {
+            time_work->start = UTC.dateTime(ISO8601);
+        }
+        if (end_work == HIGH) {
+            time_work->end = UTC.dateTime(ISO8601);
+        }
     }
 
 
